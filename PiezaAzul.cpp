f7e731f84c305dca8ee506d82f38e0bef4e4434c
Andre V <avela@unitec.edu>
2017-03-10 14:50:24 -0600

    Agregando clases de piezas

---
+++ PiezaAzul.cpp
@@ -0,0 +1,10 @@
+#include "PiezaAzul.h"
+
+
+PiezaAzul::PiezaAzul(){
+
+}
+
+PiezaAzul::PiezaAzul(char color, int posx, int posy) : Pieza(color, posx, posy){
+  
+}
---
+++ PiezaRoja.cpp
@@ -0,0 +1,28 @@
+#include "PiezaRoja.h"
+#include "PiezaRoja.h"
+#include "PiezaRoja.h"
+#include "PiezaVerde.h"
+#include "PiezaAmarilla.h"
+
+PiezaRoja::PiezaRoja(){
+
+}
+
+PiezaRoja::PiezaRoja(char color, int posx, int posy) : Pieza(color, posx, posy){
+
+}
+
+void PiezaRoja::resetPos(){
+  //posx = posicion x de inicio de pieza azul;
+  //posy = posicion y de inicio de pieza azul;
+}
+
+void PiezaRoja::comer(Pieza* pieza){
+  if(typeid(pieza).name() == typeid(PiezaAzul).name()){
+
+  }else if(typeid(pieza).name() == typeid(PiezaAmarilla).name()){
+
+  }else if(typeid(pieza).name() == typeid(PiezaVerde).name()){
+
+  }
+}
